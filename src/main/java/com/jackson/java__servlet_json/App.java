e2821c2048e2a55f580e7050afca65533daa90c3
zfc5840 <1076231986@qq.com>
2014-11-08 15:04:59 +0000

    java--servlet--json
    Change-Id: Ic3ad055464eb897e8a69c7b0187ca43c420b057d

---
+++ src/main/java/com/jackson/java__servlet_json/App.java
@@ -0,0 +1,13 @@
+package com.jackson.java__servlet_json;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
